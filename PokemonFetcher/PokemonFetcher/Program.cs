783351a207efc0a674d31854a2e053c3a3f8830e
lGruenzweilHTL <lukas.gruenzweil@liwest.at>
2024-12-14 21:26:09 +0100

    Fix type calculation

--- PokemonFetcher/PokemonFetcher/Program.cs
+++ PokemonFetcher/PokemonFetcher/Program.cs
@@ -312,156 +312,123 @@ public class Program
 
     #region Types
 
-    // Define type effectiveness
-    private static readonly Dictionary<string, Dictionary<string, double>> TypeEffectiveness = new()
+    // Define type effectiveness (outer dictionary: defender, inner dictionary: attacker)
+    private static readonly Dictionary<string, Dictionary<string, double>> TypeWeaknesses = new()
     {
         {
             "normal", new Dictionary<string, double>
             {
-                { "normal", 1 }, { "fire", 1 }, { "water", 1 }, { "electric", 1 }, { "grass", 1 }, { "ice", 1 },
-                { "fighting", 1 }, { "poison", 1 }, { "ground", 1 }, { "flying", 1 }, { "psychic", 1 }, { "bug", 1 },
-                { "rock", 0.5 }, { "ghost", 0 }, { "dragon", 1 }, { "dark", 1 }, { "steel", 0.5 }, { "fairy", 1 }
+                { "fighting", 2 }, { "ghost", 0 }
             }
         },
         {
             "fire", new Dictionary<string, double>
             {
-                { "normal", 1 }, { "fire", 0.5 }, { "water", 0.5 }, { "electric", 1 }, { "grass", 2 }, { "ice", 2 },
-                { "fighting", 1 }, { "poison", 1 }, { "ground", 1 }, { "flying", 1 }, { "psychic", 1 }, { "bug", 2 },
-                { "rock", 0.5 }, { "ghost", 1 }, { "dragon", 0.5 }, { "dark", 1 }, { "steel", 2 }, { "fairy", 1 }
+                { "fire", 0.5 }, { "water", 2 }, { "grass", 0.5 }, { "ice", 0.5 }, { "ground", 2 }, { "bug", 0.5 },
+                { "rock", 2 }, { "steel", 0.5 }, { "fairy", 0.5 }
             }
         },
         {
             "water", new Dictionary<string, double>
             {
-                { "normal", 1 }, { "fire", 2 }, { "water", 0.5 }, { "electric", 1 }, { "grass", 0.5 }, { "ice", 1 },
-                { "fighting", 1 }, { "poison", 1 }, { "ground", 2 }, { "flying", 1 }, { "psychic", 1 }, { "bug", 1 },
-                { "rock", 2 }, { "ghost", 1 }, { "dragon", 0.5 }, { "dark", 1 }, { "steel", 1 }, { "fairy", 1 }
+                { "fire", 0.5 }, { "water", 0.5 }, { "electric", 2 }, { "grass", 2 }, { "ice", 0.5 }, { "steel", 0.5 }
             }
         },
         {
             "electric", new Dictionary<string, double>
             {
-                { "normal", 1 }, { "fire", 1 }, { "water", 2 }, { "electric", 0.5 }, { "grass", 0.5 }, { "ice", 1 },
-                { "fighting", 1 }, { "poison", 1 }, { "ground", 0 }, { "flying", 2 }, { "psychic", 1 }, { "bug", 1 },
-                { "rock", 1 }, { "ghost", 1 }, { "dragon", 0.5 }, { "dark", 1 }, { "steel", 1 }, { "fairy", 1 }
+                { "electric", 0.5 }, { "ground", 2 }, { "flying", 0.5 }, { "steel", 0.5 }
             }
         },
         {
             "grass", new Dictionary<string, double>
             {
-                { "normal", 1 }, { "fire", 0.5 }, { "water", 2 }, { "electric", 1 }, { "grass", 0.5 }, { "ice", 1 },
-                { "fighting", 1 }, { "poison", 0.5 }, { "ground", 2 }, { "flying", 0.5 }, { "psychic", 1 },
-                { "bug", 0.5 }, { "rock", 2 }, { "ghost", 1 }, { "dragon", 0.5 }, { "dark", 1 }, { "steel", 0.5 },
-                { "fairy", 1 }
+                { "fire", 2 }, { "water", 0.5 }, { "electric", 0.5 }, { "grass", 0.5 }, { "ice", 2 }, { "poison", 2 },
+                { "ground", 0.5 }, { "flying", 2 }, { "bug", 2 }
             }
         },
         {
             "ice", new Dictionary<string, double>
             {
-                { "normal", 1 }, { "fire", 0.5 }, { "water", 0.5 }, { "electric", 1 }, { "grass", 2 }, { "ice", 0.5 },
-                { "fighting", 1 }, { "poison", 1 }, { "ground", 2 }, { "flying", 2 }, { "psychic", 1 }, { "bug", 1 },
-                { "rock", 1 }, { "ghost", 1 }, { "dragon", 2 }, { "dark", 1 }, { "steel", 0.5 }, { "fairy", 1 }
+                { "fire", 2 }, { "ice", 0.5 }, { "fighting", 2 }, { "rock", 2 }, { "steel", 2 }
             }
         },
         {
             "fighting", new Dictionary<string, double>
             {
-                { "normal", 2 }, { "fire", 1 }, { "water", 1 }, { "electric", 1 }, { "grass", 1 }, { "ice", 2 },
-                { "fighting", 1 }, { "poison", 0.5 }, { "ground", 1 }, { "flying", 0.5 }, { "psychic", 0.5 },
-                { "bug", 0.5 }, { "rock", 2 }, { "ghost", 0 }, { "dragon", 1 }, { "dark", 2 }, { "steel", 2 },
-                { "fairy", 0.5 }
+                { "flying", 2 }, { "psychic", 2 }, { "bug", 0.5 }, { "rock", 0.5 }, { "dark", 0.5 }, { "fairy", 2 }
             }
         },
         {
             "poison", new Dictionary<string, double>
             {
-                { "normal", 1 }, { "fire", 1 }, { "water", 1 }, { "electric", 1 }, { "grass", 2 }, { "ice", 1 },
-                { "fighting", 1 }, { "poison", 0.5 }, { "ground", 0.5 }, { "flying", 1 }, { "psychic", 1 },
-                { "bug", 1 }, { "rock", 0.5 }, { "ghost", 0.5 }, { "dragon", 1 }, { "dark", 1 }, { "steel", 0 },
-                { "fairy", 2 }
+                { "grass", 0.5 }, { "fighting", 0.5 }, { "poison", 0.5 }, { "ground", 2 }, { "psychic", 2 },
+                { "bug", 0.5 }, { "fairy", 0.5 }
             }
         },
         {
             "ground", new Dictionary<string, double>
             {
-                { "normal", 1 }, { "fire", 2 }, { "water", 1 }, { "electric", 2 }, { "grass", 0.5 }, { "ice", 1 },
-                { "fighting", 1 }, { "poison", 2 }, { "ground", 1 }, { "flying", 0 }, { "psychic", 1 }, { "bug", 0.5 },
-                { "rock", 2 }, { "ghost", 1 }, { "dragon", 1 }, { "dark", 1 }, { "steel", 2 }, { "fairy", 1 }
+                { "water", 2 }, { "grass", 2 }, { "ice", 2 }, { "poison", 0.5 }, { "rock", 0.5 }, { "electric", 0 }
             }
         },
         {
             "flying", new Dictionary<string, double>
             {
-                { "normal", 1 }, { "fire", 1 }, { "water", 1 }, { "electric", 0.5 }, { "grass", 2 }, { "ice", 1 },
-                { "fighting", 2 }, { "poison", 1 }, { "ground", 0 }, { "flying", 1 }, { "psychic", 1 }, { "bug", 2 },
-                { "rock", 0.5 }, { "ghost", 1 }, { "dragon", 1 }, { "dark", 1 }, { "steel", 0.5 }, { "fairy", 1 }
+                { "electric", 2 }, { "grass", 0.5 }, { "fighting", 0.5 }, { "bug", 0.5 }, { "rock", 2 }, { "ice", 2 },
+                { "ground", 0 }
             }
         },
         {
             "psychic", new Dictionary<string, double>
             {
-                { "normal", 1 }, { "fire", 1 }, { "water", 1 }, { "electric", 1 }, { "grass", 1 }, { "ice", 1 },
-                { "fighting", 2 }, { "poison", 2 }, { "ground", 1 }, { "flying", 1 }, { "psychic", 0.5 }, { "bug", 1 },
-                { "rock", 1 }, { "ghost", 1 }, { "dragon", 1 }, { "dark", 0 }, { "steel", 0.5 }, { "fairy", 1 }
+                { "fighting", 0.5 }, { "psychic", 0.5 }, { "bug", 2 }, { "ghost", 2 }, { "dark", 2 }
             }
         },
         {
             "bug", new Dictionary<string, double>
             {
-                { "normal", 1 }, { "fire", 0.5 }, { "water", 1 }, { "electric", 1 }, { "grass", 2 }, { "ice", 1 },
-                { "fighting", 0.5 }, { "poison", 0.5 }, { "ground", 1 }, { "flying", 0.5 }, { "psychic", 2 },
-                { "bug", 1 }, { "rock", 1 }, { "ghost", 0.5 }, { "dragon", 1 }, { "dark", 2 }, { "steel", 0.5 },
-                { "fairy", 0.5 }
+                { "fire", 2 }, { "grass", 0.5 }, { "fighting", 0.5 }, { "ground", 0.5 }, { "flying", 2 }, { "rock", 2 }
             }
         },
         {
             "rock", new Dictionary<string, double>
             {
-                { "normal", 1 }, { "fire", 2 }, { "water", 1 }, { "electric", 1 }, { "grass", 1 }, { "ice", 2 },
-                { "fighting", 0.5 }, { "poison", 1 }, { "ground", 0.5 }, { "flying", 2 }, { "psychic", 1 },
-                { "bug", 2 }, { "rock", 1 }, { "ghost", 1 }, { "dragon", 1 }, { "dark", 1 }, { "steel", 0.5 },
-                { "fairy", 1 }
+                { "normal", 0.5 }, { "fire", 0.5 }, { "water", 2 }, { "grass", 2 }, { "fighting", 2 },
+                { "poison", 0.5 }, { "ground", 2 }, { "flying", 0.5 }, { "steel", 2 }
             }
         },
         {
             "ghost", new Dictionary<string, double>
             {
-                { "normal", 0 }, { "fire", 1 }, { "water", 1 }, { "electric", 1 }, { "grass", 1 }, { "ice", 1 },
-                { "fighting", 1 }, { "poison", 1 }, { "ground", 1 }, { "flying", 1 }, { "psychic", 2 }, { "bug", 1 },
-                { "rock", 1 }, { "ghost", 2 }, { "dragon", 1 }, { "dark", 0.5 }, { "steel", 1 }, { "fairy", 1 }
+                { "normal", 0 }, { "fighting", 0 }, { "poison", 0.5 }, { "bug", 0.5 }, { "ghost", 2 }, { "dark", 2 }
             }
         },
         {
             "dragon", new Dictionary<string, double>
             {
-                { "normal", 1 }, { "fire", 1 }, { "water", 1 }, { "electric", 1 }, { "grass", 1 }, { "ice", 1 },
-                { "fighting", 1 }, { "poison", 1 }, { "ground", 1 }, { "flying", 1 }, { "psychic", 1 }, { "bug", 1 },
-                { "rock", 1 }, { "ghost", 1 }, { "dragon", 2 }, { "dark", 1 }, { "steel", 0.5 }, { "fairy", 2 }
+                { "fire", 0.5 }, { "water", 0.5 }, { "grass", 0.5 }, { "electric", 0.5 }, { "ice", 2 }, { "dragon", 2 },
+                { "fairy", 2 }
             }
         },
         {
             "dark", new Dictionary<string, double>
             {
-                { "normal", 1 }, { "fire", 1 }, { "water", 1 }, { "electric", 1 }, { "grass", 1 }, { "ice", 1 },
-                { "fighting", 0.5 }, { "poison", 1 }, { "ground", 1 }, { "flying", 1 }, { "psychic", 2 }, { "bug", 1 },
-                { "rock", 1 }, { "ghost", 2 }, { "dragon", 1 }, { "dark", 0.5 }, { "steel", 1 }, { "fairy", 0.5 }
+                { "fighting", 2 }, { "psychic", 0 }, { "bug", 2 }, { "ghost", 0.5 }, { "dark", 0.5 }, { "fairy", 2 }
             }
         },
         {
             "steel", new Dictionary<string, double>
             {
-                { "normal", 1 }, { "fire", 0.5 }, { "water", 0.5 }, { "electric", 0.5 }, { "grass", 1 }, { "ice", 2 },
-                { "fighting", 1 }, { "poison", 1 }, { "ground", 1 }, { "flying", 1 }, { "psychic", 1 }, { "bug", 1 },
-                { "rock", 2 }, { "ghost", 1 }, { "dragon", 1 }, { "dark", 1 }, { "steel", 0.5 }, { "fairy", 2 }
+                { "normal", 0.5 }, { "fire", 2 }, { "grass", 0.5 }, { "ice", 0.5 }, { "fighting", 2 }, { "poison", 0 },
+                { "ground", 2 }, { "flying", 0.5 }, { "psychic", 0.5 }, { "bug", 0.5 }, { "rock", 0.5 },
+                { "dragon", 0.5 }, { "steel", 0.5 }, { "fairy", 0.5 }
             }
         },
         {
             "fairy", new Dictionary<string, double>
             {
-                { "normal", 1 }, { "fire", 0.5 }, { "water", 1 }, { "electric", 1 }, { "grass", 1 }, { "ice", 1 },
-                { "fighting", 2 }, { "poison", 0.5 }, { "ground", 1 }, { "flying", 1 }, { "psychic", 1 }, { "bug", 1 },
-                { "rock", 1 }, { "ghost", 1 }, { "dragon", 2 }, { "dark", 2 }, { "steel", 0.5 }
+                { "fighting", 0.5 }, { "poison", 2 }, { "bug", 0.5 }, { "dragon", 0 }, { "dark", 0.5 }, { "steel", 2 }
             }
         }
     };
@@ -473,13 +440,19 @@ public class Program
 
         foreach (var type in types)
         {
-            foreach (var (targetType, multiplier) in TypeEffectiveness[type])
+            foreach (var (targetType, multiplier) in TypeWeaknesses[type])
             {
                 if (!effectiveness.TryAdd(targetType, multiplier))
                 {
                     effectiveness[targetType] *= multiplier;
                 }
             }
+        }
+
+        // Ensure all types have at least a neutral effectiveness
+        foreach (var type in TypeWeaknesses.Keys)
+        {
+            effectiveness.TryAdd(type, 1);
         }
 
         return effectiveness;
@@ -491,7 +464,7 @@ public class Program
         var table = new StringBuilder();
         table.Append("<tr>");
 
-        var categories = new[] { 0.0, 0.25, 0.5, 1.0, 2.0, 4.0 };
+        var categories = new[] { 0, 0.25, 0.5, 1, 2, 4 };
         foreach (var category in categories)
         {
             table.Append("<td>");
@@ -523,7 +496,7 @@ public class Program
                     new KeyValuePair<string, string[]>(null, null))
             let location = region.Value?.Where(area.Name.StartsWith).MaxBy(a => a.Length)
             group (area.Name, location) by region.Key;
-        
+
         if (!groupedLocations.Any()) return "<p>None</p>";
 
         string relativePath = GetRelativePath(fileDirectory, drawingPath);
